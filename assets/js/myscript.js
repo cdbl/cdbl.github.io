8674a81ceae3231a98eceadcd57d33889bf937d0
cdbl <cdblynser@icloud.com>
2022-10-30 10:44:14 +0530

    update
    Added JavaScript and rearranged folder system

---
+++ assets/js/myscript.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  //do work
+  var nav_link_start = '<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">' +
+          '<div class="container-fluid">' +
+            '<a class="navbar-brand" href="index.html">[CHWAME]</a>' +
+            '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">' +
+              '<span class="navbar-toggler-icon"></span>' +
+            '</button>' +
+            '<div class="collapse navbar-collapse" id="navbarCollapse">' +
+              '<ul class="navbar-nav me-auto mb-2 mb-md-0">';
+  var nav_link_end = '</ul></div></div></nav>';
+
+  var links = [
+      {link:"./index.html", title:"Home"},
+      {link: "./hobbies.html", title: "My Hobbies"},
+      {link: "./stories.html", title: "My Stories"}
+  ];
+
+  var htmlLinks = "";
+  links.forEach(function(value){
+      htmlLinks += '<li class="nav-item"><a href="' + value.link + '" class="nav-link">' + value.title + '</a></li>';
+  })
+
+  document.getElementById("header").innerHTML = nav_link_start + htmlLinks + nav_link_end;
+});
