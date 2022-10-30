9061890959174c3d2b8f73de7d8d688a244af481
cdbl <cdblynser@icloud.com>
2022-10-30 13:26:36 +0530

    Update myscript.js

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //do work
   var nav_link_start = '<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">' +
           '<div class="container-fluid">' +
-            '<a class="navbar-brand" href="index.html">[CHWAME]</a>' +
+            '<a class="navbar-brand" href="/">[CHWAME]</a>' +
             '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">' +
               '<span class="navbar-toggler-icon"></span>' +
             '</button>' +
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var nav_link_end = '</ul></div></div></nav>';
 
   var links = [
-      {link:"index.html", title:"Home"},
+      {link:"/", title:"Home"},
       {link: "hobbies.html", title: "My Hobbies"},
       {link: "stories.html", title: "My Stories"}
   ];
